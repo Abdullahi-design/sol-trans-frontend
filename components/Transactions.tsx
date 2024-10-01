1658bc4b1357b3d4a1d2635890ad79614f9ae75e
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-10-01 06:59:54 +0100

    added spinner loader

--- components/Transactions.tsx
+++ components/Transactions.tsx
@@ -4,6 +4,7 @@ import { recentTransactionsWebSocket } from '@/utils/action';
 import { useWallet } from '@solana/wallet-adapter-react';
 import React, { useEffect, useState } from 'react';
 import { FiCheckCircle, FiClock, FiCopy } from 'react-icons/fi'; // Import icons
+import SpinnerLoader from './SpinnerLoader';
 
 // Define a custom transaction type to avoid conflicts with other types
 interface CustomTransaction {
@@ -21,6 +22,7 @@ const Transactions = () => {
   const [transactions, setTransactions] = useState<CustomTransaction[]>([]); // Define state with the correct type
   const [copiedText, setCopiedText] = useState<string | null>(null);
   const [mountPublicKey, setMountPublicKey] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     // Only set mountPublicKey if publicKey is available
@@ -29,6 +31,8 @@ const Transactions = () => {
     }
 
     const fetchTransactions = async () => {
+      setLoading(true); 
+
       if (mountPublicKey) {
         try {
           const response = await recentTransactionsWebSocket(mountPublicKey.toBase58());
@@ -39,6 +43,8 @@ const Transactions = () => {
           }
         } catch (error) {
           console.error('Error fetching recent transactions:', error);
+        } finally {
+          setLoading(false);
         }
       }
     };
@@ -63,63 +69,71 @@ const Transactions = () => {
     <div className="h-fit bg-[#f5f7fa] mt-20 flex flex-col items-center justify-center p-4">
       {/* <h1 className="text-2xl font-bold mb-6">Recent Transactions</h1> */}
       
-      {transactions.length === 0 ? (
-        <p className="text-gray-600">No transactions found</p>
+      {loading ? (
+        <div className="flex items-center justify-center h-96">
+          <SpinnerLoader />
+        </div>
       ) : (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-5xl h-[80vh] overflow-y-scroll">
-          {transactions.map((transaction, index) => (
-            <li key={index} className="bg-white shadow-lg rounded-lg p-6 border-t-4 border-teal-500">
-              <div className="flex items-center justify-center w-full mb-2">
-                <div className="flex items-center space-x-2">
-                  {transaction.status === 'finalized' ? (
-                    <FiCheckCircle className="text-green-500" size={24} />
-                  ) : (
-                    <FiClock className="text-yellow-500" size={24} />
-                  )}
-                  <h2 className="text-xl font-semibold">Transaction {index + 1}</h2>
-                </div>
-              </div>
-                <span className="text-gray-500 text-sm">{transaction.time}</span>
-              <div className="flex items-center justify-between">
-                <p className="mt-2 text-gray-700 truncate">
-                  <strong>Signature:</strong> {transaction.signature.substring(0, 10)}... {/* Clamp text */}
-                </p>
-                <button
-                  onClick={() => copyToClipboard(transaction.signature)}
-                  className="text-gray-500 hover:text-teal-500 ml-2"
-                >
-                  <FiCopy size={20} />
-                </button>
-              </div>
-              {copiedText === transaction.signature && (
-                <p className="text-green-500 text-sm mt-1">Signature copied!</p>
-              )}
-              <p className={`mt-1 text-sm ${transaction.status === 'finalized' ? 'text-green-500' : 'text-yellow-500'}`}>
-                <strong>Status:</strong> {transaction.status}
-              </p>
-              <div className="mt-4">
-                <h3 className="font-semibold mb-2">Instructions:</h3>
-                <ul className="list-disc pl-4">
-                  {transaction.instructions.map((instruction) => (
-                    <li key={instruction.instructionNumber} className="truncate">
-                      <span className="text-gray-600">Instruction {instruction.instructionNumber}: </span>
-                      <span className="text-gray-800">{instruction.programId.substring(0, 10)}...</span> {/* Clamp text */}
-                      <button
-                        onClick={() => copyToClipboard(instruction.programId)}
-                        className="text-gray-500 hover:text-teal-500 ml-2"
-                      >
-                        <FiCopy size={20} />
-                      </button>
-                      {copiedText === instruction.programId && (
-                        <p className="text-green-500 text-sm">Instruction copied!</p>
+        <>
+          {transactions.length === 0 ? (
+            <p className="text-gray-600">No transactions found</p>
+          ) : (
+            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-5xl h-[80vh] overflow-y-scroll scrollbar-thin scrollbar-thumb-teal-500 scrollbar-track-gray-100 hover:scrollbar-thumb-teal-600">
+              {transactions.map((transaction, index) => (
+                <li key={index} className="bg-white shadow-lg rounded-lg p-6 border-t-4 border-teal-500">
+                  <div className="flex items-center justify-center w-full mb-2">
+                    <div className="flex items-center space-x-2">
+                      {transaction.status === 'finalized' ? (
+                        <FiCheckCircle className="text-green-500" size={24} />
+                      ) : (
+                        <FiClock className="text-yellow-500" size={24} />
                       )}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </li>
-          ))}
-        </ul>
+                      <h2 className="text-xl font-semibold">Transaction {index + 1}</h2>
+                    </div>
+                  </div>
+                    <span className="text-gray-500 text-sm">{transaction.time}</span>
+                  <div className="flex items-center justify-between">
+                    <p className="mt-2 text-gray-700 truncate">
+                      <strong>Signature:</strong> {transaction.signature.substring(0, 10)}... {/* Clamp text */}
+                    </p>
+                    <button
+                      onClick={() => copyToClipboard(transaction.signature)}
+                      className="text-gray-500 hover:text-teal-500 ml-2"
+                    >
+                      <FiCopy size={20} />
+                    </button>
+                  </div>
+                  {copiedText === transaction.signature && (
+                    <p className="text-green-500 text-sm mt-1">Signature copied!</p>
+                  )}
+                  <p className={`mt-1 text-sm ${transaction.status === 'finalized' ? 'text-green-500' : 'text-yellow-500'}`}>
+                    <strong>Status:</strong> {transaction.status}
+                  </p>
+                  <div className="mt-4">
+                    <h3 className="font-semibold mb-2">Instructions:</h3>
+                    <ul className="list-disc pl-4">
+                      {transaction.instructions.map((instruction) => (
+                        <li key={instruction.instructionNumber} className="truncate">
+                          <span className="text-gray-600">Instruction {instruction.instructionNumber}: </span>
+                          <span className="text-gray-800">{instruction.programId.substring(0, 10)}...</span> {/* Clamp text */}
+                          <button
+                            onClick={() => copyToClipboard(instruction.programId)}
+                            className="text-gray-500 hover:text-teal-500 ml-2"
+                          >
+                            <FiCopy size={20} />
+                          </button>
+                          {copiedText === instruction.programId && (
+                            <p className="text-green-500 text-sm">Instruction copied!</p>
+                          )}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
       )}
     </div>
   );
